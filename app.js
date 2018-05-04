0737fdd04b52d32b29e667c0fba8088d1fa89fa8
afsanchez <36503798+afsanchez@users.noreply.github.com>
2018-05-04 17:09:28 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
 
 var app = express();
 
@@ -14,17 +14,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
-var app = express();
-
 var router = express.Router();
-app.use(credits);
-app.use(quizzes);
-
-router.get(GET/credits);
-router.get(GET/quizzes);
+const Sequelize=require("sequelize");
+const {models}=require("../models");
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Quiz' });
+});
+/* GET creditos. */
+router.get('/credits', function(req, res, next) {
+  res.render('credits', { title: 'Credits' });
+});
+/* GET quizzes. */
+router.get('/quizzes', function(req, res, next) {
+	models.quiz.findAll()
+	.then(quizzes => {
+		res.render('quizzes', { quizzes } );
+	})
+  	.catch(error => {
+  		next(error)
+  	});
+});
+module.exports = router;
