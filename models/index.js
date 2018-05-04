950bf6ffb82647bce3ff97d9ad9fca477072450e
afsanchez <36503798+afsanchez@users.noreply.github.com>
2018-05-04 17:14:10 +0200

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,33 +1,32 @@
 const Sequelize = require('sequelize');
+
 const sequelize = new Sequelize("sqlite:quizzes.sqlite", {logging: false});
 
-sequelize.define(
-	'quiz',
-	{ question: {
+sequelize.define('quiz', {
+	question: {
 		type: Sequelize.STRING,
-		unique: { msg: "ya existe esta pregunta"},
+		unique: {msg: "Ya existe esta pregunta"},
 		validate: {notEmpty: {msg: "La pregunta no puede estar vacía"}}
-	  },
-	  answer: {
-	  	type: Sequelize.STRING,
-	  	validate: {notEmpty: {msg: "La respuesta no puede estar vacía"}}
-	  } 
+	},
+	answer: {
+		type: Sequelize.STRING,
+		validate: {notEmpty: {msg: "La respuesta no puede estar vacía"}}
 	}
-);
+});
 
 sequelize.sync()
 .then(() => sequelize.models.quiz.count())
 .then(count => {
-	if(!count){
+	if (!count) {
 		return sequelize.models.quiz.bulkCreate([
-			{question: "Capital de Italia", answer: "Roma"},
-			{question: "Capital de Francia", answer: "París"},
-			{question: "Capital de España", answer: "Madrid"},
-			{question: "Capital de Portugal", answer: "Lisboa"}
+		{question: "Capital de Italia", answer: "Roma"},
+		{question: "Capital de Francia", answer: "Paris"},
+		{question: "Capital de España", answer: "Madrid"},
+		{question: "Capital de Portugal", answer: "Lisboa"}		
 		]);
 	}
 })
-.catch(error =>{
+.catch(error => {
 	console.log(error);
 });
 
